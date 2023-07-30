a5c4128bb62142c8f80aaf812548ca5fca80b7c5
RT <rustytrim11@gmail.com>
2023-07-30 00:46:04 -0400

    Exports updated

--- src/index.js
+++ src/index.js
@@ -1,6 +1,36 @@
-'use-strict';
+"use-strict";
+// Client
 exports.BaseClient = require("./client/BaseClient");
 exports.Client = require("./client/Client");
-exports.ClientUser = require("./structures/ClientUser");
-exports.GatewayIntents = require("./gateway/GatewayIntents");
 exports.WebSocketManager = require("./client/ws/WebSocketManager");
+
+// Config
+exports.OpCodes = require("./config/OpCodes");
+
+// Gateway
+exports.GatewayIntents = require("./gateway/GatewayIntents");
+
+// Event Handlers
+exports.ReadyEventHandler = require("./handlers/client/ReadyEventHandler");
+exports.MessageCreateEventHandler = require("./handlers/client/MessageCreateEventHandler");
+
+// Managers
+exports.CachedManager = require("./managers/CachedManager");
+exports.ChannelManager = require("./managers/ChannelManager");
+exports.GuildManager = require("./managers/GuildManager");
+exports.UserManager = require("./managers/UserManager");
+
+// Structures
+exports.Channel = require("./structures/Channel");
+exports.ClientUser = require("./structures/ClientUser");
+exports.DMChannel = require("./structures/DMChannel");
+exports.Guild = require("./structures/Guild");
+exports.Message = require("./structures/Message");
+exports.Role = require("./structures/Role");
+exports.TextChannel = require("./structures/TextChannel");
+exports.ThreadChannel = require("./structures/ThreadChannel");
+exports.User = require("./structures/User");
+exports.VoiceChannel = require("./structures/VoiceChannel");
+
+// Utils
+exports.Collection = require("./utils/Collection");
